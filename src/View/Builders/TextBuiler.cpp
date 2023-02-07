8d3fc896b4d8b02c24eafc0be25d4667fd952c06
kevin1892 <kevin.zevallos@utec.edu.pe>
2023-02-07 18:18:10 -0500

    TextBuilder added with default font and custom one;

---
+++ src/View/Builders/TextBuiler.cpp
@@ -0,0 +1,47 @@
+#include "TextBuiler.hpp"
+#include <string>
+#include <Font.hpp>
+#include <Text.hpp>
+TextBuilder::TextBuilder() {
+    position = { 0, 0 };
+    fontSize = 10;
+    color = WHITE;
+    font = GetFontDefault();
+}
+
+TextBuilder &TextBuilder::Text(std::string text_) {
+    this->text = text_;
+    return *this;
+}
+TextBuilder &TextBuilder::Position(Vector2 position_) {
+    this->position = position_;
+    return *this;
+}
+TextBuilder &TextBuilder::FontSize(int fontSize_){
+    this->fontSize = fontSize_;
+    return *this;
+}
+TextBuilder &TextBuilder::Color_(Color color_) {
+    this->color = color_;
+    return *this;
+}
+TextBuilder &TextBuilder::Font_(Font font_) {
+    this->font = font_;
+    return *this;
+}
+
+void TextBuilder::draw() {
+    {
+        DrawTextEx(font, text.c_str(), position, fontSize, 0.0f, color);
+    }
+}
+//EXAMPLE
+// Inicializar la ventana de la aplicación
+//Font customFont = LoadFont("path/to/font.ttf");
+
+//TextBuilder builder;
+//builder.setText("Hello World!")
+//.Position({ 100, 100 })
+//.FontSize(20)
+//.Color(RED)
+//.Font(customFont);
--- src/View/Builders/TextBuiler.hpp
+++ src/View/Builders/TextBuiler.hpp
@@ -4,17 +4,29 @@
 #include <Font.hpp>
 #include <Text.hpp>
 
-class FontBuilder {
-public:
+class TextBuilder
+{
 private:
-};
+    std::string text;
+    Vector2 position;
+    int fontSize;
+    Color color;
+    Font font;
 
-class TextBuilder {
 public:
-  // casting to raylib::Text
-  operator raylib::Text() const;
+    TextBuilder();
 
-private:
+    TextBuilder& Text(std::string);
+
+    TextBuilder& Position(Vector2 position_);
+
+    TextBuilder& FontSize(int fontSize_);
+
+    TextBuilder& Color_(Color color_);
+
+    TextBuilder& Font_(Font font_);
+
+    void draw();
+
 };
-
 #endif // !TEXT_BUILDER_HPP
