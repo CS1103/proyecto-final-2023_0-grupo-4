a5fb7675b20efb0ac1fab0ebdf62a12ce4a87b04
Enriquefft <enriquefft2001@gmail.com>
2023-02-25 22:32:48 -0500

    updated docs

--- docs/search/searchdata.js
+++ docs/search/searchdata.js
@@ -1,42 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "_abcdefghikmnoprstuvwxy",
-  1: "bcfghkmstvw",
-  2: "dkuw",
-  3: "bcgkmstuvw",
-  4: "_bcdghimnoprstuv",
-  5: "_bdeghmnprstvwxy",
-  6: "dmsu",
-  7: "ahs",
-  8: "abcdegprsw",
-  9: "i"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "namespaces",
-  3: "files",
-  4: "functions",
-  5: "variables",
-  6: "typedefs",
-  7: "enums",
-  8: "enumvalues",
-  9: "concepts"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Classes",
-  2: "Namespaces",
-  3: "Files",
-  4: "Functions",
-  5: "Variables",
-  6: "Typedefs",
-  7: "Enumerations",
-  8: "Enumerator",
-  9: "Concepts"
 };
 
